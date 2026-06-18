319ad0713b45d118b669a1c71b5bfa77d7c90771
badmon888 <79205729+badmon888@users.noreply.github.com>
2026-06-18 14:34:02 +0800

    Update 海阔采集站.js
    保存到本地

--- 海阔采集站.js
+++ 海阔采集站.js
@@ -184,11 +184,18 @@ function 单转双pid(list, key) {
 
 function 跳转(str) {
   const 跳转 = $('hiker://empty##fypage').rule(
-    (file, str) => {
+    (lp, str) => {
+      if(!fileExist(lp+"海阔采集站.js")){
+        file=lp+"海阔采集站.js";
+      }else{
+        file="https://v4.gh-proxy.org/https://raw.githubusercontent.com/badmon888/monkeyjs/refs/heads/main/%E6%B5%B7%E9%98%94%E9%87%87%E9%9B%86%E7%AB%99.js";
+        writeFile(lp+"海阔采集站.js",fetch(file));
+        
+      }
       const 函数 = $.require(file);
       函数[str]();
     },
-    localPath + "函数.js",
+    localPath,
     str
   );
   return 跳转;
